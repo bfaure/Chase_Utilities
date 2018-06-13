42430c34c634a2b098746134b4239b7fd5072d15
Brian Faure <bfaure23@gmail.com>
2018-06-12 20:02:54 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime as dt
+
+data_f='Chase5839_Activity_20180612.CSV'
+
+data=open(data_f,'r').read().split('\n')
+
+date,desc,amount,balance=[],[],[],[]
+
+for d in data:
+	e=d.split(',')
+	if len(e)>2:
+		try:
+			balance=[float(e[5])]+balance
+			date=[e[1]]+date
+			desc=[e[2]]+desc
+			amount=[float(e[3])]+amount
+		except:
+			continue
+
+def plot(balance):
+	plt.plot(balance)
+	plt.ylabel('Balance $')
+	plt.show()
+
+def spent_made(amount,date):
+	tot_spent=0.0
+	tot_made=0.0
+	for a in amount:
+		if a<0: tot_spent-=a
+		else: tot_made+=a
+	print "\n","="*25
+	print "Between %s and %s..."%(date[0],date[-1])
+	print "Total amount spent: $%0.2f"%tot_spent
+	print "Total amount made:  $%0.2f"%tot_made
+	print "="*25
+
+spent_made(amount,date)
+plot(balance)
+
+
